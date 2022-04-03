f9e3723cb60014c4cba87cfd0e1888fd671581de
TokarevS <snake_84@mail.ru>
2022-04-03 20:32:52 +0500

    #online_1

--- main.cpp
+++ main.cpp
@@ -1,103 +1,106 @@
 #include <iostream>
 
-class Cat {
+class Animal{
+protected:
     int age;
-    mutable std::string name;
+    std::string name;
     std::string color;
-
+    Animal(){}
 public:
-    Cat() : age(0) {
-        color = "Peachy";
-        std::cout << " MEOW" << std::endl;
-    };
 
-    Cat(std::string name, std::string color, int age) {
+    Animal(std::string name, std::string color, int age) {
         this->name = name;
         this->color = color;
         this->age = age;
     }
 
-    //Copy constructor
-    Cat(Cat& c)
-            : name(c.name), color(c.color), age(c.age) {}
-
-    ~Cat(){
-        std::cout << "Cat left us..." << std::endl;
+    ~Animal(){
+        std::cout << "Animal left us..." << std::endl;
     }
 
-    void voice(){
-        std::cout << name << " meow" << std::endl;
-    }
+    void setAge(int a){ age = a; }
+    void setName(std::string n) { name = n; }
+    void setColor(std::string n) { color = n; }
+    int getAge () const { return age; }
 
-    void move(){
-        std::cout << name << " walks on paws" << std::endl;
-    }
+    std::string getName() const { return name; }
+    std::string getColor() const { return color; }
 
-    void setAge(int a){
-        age = a;
-    }
-
-    void setName(std::string n) const {
-        name = n;
-    }
-
-    void const setColor(std::string n){
-        color = n;
-    }
-
-    int getAge(){
-        return age;
-    }
-
-    std::string getName(){
-        return name;
-    }
-
-    std::string getColor(){
-        return color;
-    }
-
+    void move(){ std::cout << name << " walks on paws" << std::endl; }
     void info() {
         printf("%s is %s and %d years of age\n", name.c_str(), color.c_str(), age);
     }
 };
 
-void makeCatMove(Cat* c){
-    (*c).move();
-}
 
-void makeCatMove(Cat& c){
-    c.move();
-}
+class Cat : public Animal{
+public:
+    Cat(std::string name, std::string color, int age) : Animal(name, color, age) {}
+
+    virtual ~Cat(){ std::cout << "Cat left us..." << std::endl; }
+
+    void voice(){ std::cout << name << " meow" << std::endl; }
+};
+
+
+class Dog : public Animal{
+public:
+    Dog(std::string name, std::string color, int age) {
+        this->name = name;
+        this->color = color;
+        this->age = age;
+    }
+
+    virtual ~Dog(){ std::cout << "Dog left us..." << std::endl; }
+
+    void voice(){ std::cout << name << " woof" << std::endl; }
+};
+
+
+class Bird : public Animal{
+protected:
+    int flyHeight;
+public:
+    Bird(std::string name, std::string color, int age, int flyHeight) : Animal(name, color, age) {
+        this->flyHeight = flyHeight;
+    }
+    virtual ~Bird(){ std::cout << "Bird left us..." << std::endl; }
+    void voice(){ std::cout << name << " tweet" << std::endl; }
+};
+
+class Parrot : public Bird/*, public Cat*/{
+public:
+    //Parrot(std::string name, std::string color, int age, int height) : Animal(name, color, age), flyHeight(height) {}
+    Parrot(std::string name, std::string color, int age, int height) : Bird(name, color, age, height) {}
+    virtual ~Parrot(){ std::cout << "Parrot left us..." << std::endl; }
+    void speak(){ std::cout << "polly wanna cracker" << std::endl; }
+};
+
 
 int main() {
-    // classes vs objects
-    Cat c0("Barsik", "White", 5);
 
-    c0.info();
+    Cat c0("Barsik", "Black", 2);
+    Cat c1("Tuzik", "Gray", 3);
+    Dog d0("Sharik", "White", 4);
+    Dog d1("Bobik", "Blue", 1);
+    Bird b0("Chijik", "Yellow", 1, 5);
+    Parrot p1("Polly", "Green", 2, 3);
 
-    Cat c1("Murzik", "Black", 2);
+    Animal * zoo[] = {&c0, &c1, &d0, &d1, &b0, &p1};
+    for (int i = 0; i < 6; i++) {
+        zoo[i]->info();
 
-    c1.info();
-//    c0.setAge(2);
-//    c0.setName("Murzik");
-//    c0.setColor("Black");
+    }
 
-    c0.voice();
-    c0.move();
+    //typecast
+    Parrot* pp = static_cast<Parrot*>(zoo[5]);
 
-    Cat c2;
-    c2.setName("Kotik");
-    c2.info();
+    pp->speak();
 
-    Cat* c2p = &c2;
+    static_cast<Parrot *> (zoo[5])->speak();
 
-    c2p->setName("pointer");
-    c2.info();
+    ((Parrot *) zoo[5])->speak();
 
-    Cat c4(c1);
-    c4.info();
-
-    //std::cout << "Hello, World!" << std::endl;
+    p1.speak();
     return 0;
 }
