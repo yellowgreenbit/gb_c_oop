a93438fdc1087800f6df35beaaf0c3472bff1b1a
TokarevS <snake_84@mail.ru>
2022-04-05 22:34:18 +0500

    #homework_3

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ protected:
     int age;
     std::string name;
     std::string color;
-    Animal(){}
 public:
 
     Animal(std::string name, std::string color, int age) {
@@ -17,14 +16,6 @@ public:
     ~Animal(){
         std::cout << "Animal left us..." << std::endl;
     }
-
-    void setAge(int a){ age = a; }
-    void setName(std::string n) { name = n; }
-    void setColor(std::string n) { color = n; }
-    int getAge () const { return age; }
-
-    std::string getName() const { return name; }
-    std::string getColor() const { return color; }
 
     void move(){ std::cout << name << " walks on paws" << std::endl; }
     void info() {
@@ -38,21 +29,15 @@ public:
     Cat(std::string name, std::string color, int age) : Animal(name, color, age) {}
 
     virtual ~Cat(){ std::cout << "Cat left us..." << std::endl; }
-
     void voice(){ std::cout << name << " meow" << std::endl; }
 };
 
 
 class Dog : public Animal{
 public:
-    Dog(std::string name, std::string color, int age) {
-        this->name = name;
-        this->color = color;
-        this->age = age;
-    }
+    Dog(std::string name, std::string color, int age) : Animal(name, color, age) {}
 
     virtual ~Dog(){ std::cout << "Dog left us..." << std::endl; }
-
     void voice(){ std::cout << name << " woof" << std::endl; }
 };
 
@@ -70,7 +55,6 @@ public:
 
 class Parrot : public Bird/*, public Cat*/{
 public:
-    //Parrot(std::string name, std::string color, int age, int height) : Animal(name, color, age), flyHeight(height) {}
     Parrot(std::string name, std::string color, int age, int height) : Bird(name, color, age, height) {}
     virtual ~Parrot(){ std::cout << "Parrot left us..." << std::endl; }
     void speak(){ std::cout << "polly wanna cracker" << std::endl; }
@@ -78,29 +62,5 @@ public:
 
 
 int main() {
-
-    Cat c0("Barsik", "Black", 2);
-    Cat c1("Tuzik", "Gray", 3);
-    Dog d0("Sharik", "White", 4);
-    Dog d1("Bobik", "Blue", 1);
-    Bird b0("Chijik", "Yellow", 1, 5);
-    Parrot p1("Polly", "Green", 2, 3);
-
-    Animal * zoo[] = {&c0, &c1, &d0, &d1, &b0, &p1};
-    for (int i = 0; i < 6; i++) {
-        zoo[i]->info();
-
-    }
-
-    //typecast
-    Parrot* pp = static_cast<Parrot*>(zoo[5]);
-
-    pp->speak();
-
-    static_cast<Parrot *> (zoo[5])->speak();
-
-    ((Parrot *) zoo[5])->speak();
-
-    p1.speak();
     return 0;
 }
